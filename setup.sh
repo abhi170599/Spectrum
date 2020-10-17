9a8c0aad80f5addc7e88eaac6ff555a1b790a0a0
Abhishek Jha <abhi170599@gmail.com>
2020-10-17 16:35:55 +0530

    added setup file

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# installing docker
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - 
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# installing docker compose
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+# setting up docker host
+
+sudo ls -la /var/run/docker.sock
+sudo usermod -aG docker ${USER}
